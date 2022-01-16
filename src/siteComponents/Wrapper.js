488cfb03328242af52c9dd1416344adc60da0c1e
Arthur Fincham <arthurfincham@me.com>
2022-01-16 09:55:23 +0000

    add react-spring

--- src/siteComponents/Wrapper.js
+++ src/siteComponents/Wrapper.js
@@ -1,15 +1,12 @@
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { materialLight } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { CodeBlock } from 'react-code-blocks';
 
 export default function Wrapper({ compChild, title, myCode }) {
   return (
-    <div className="flex min-h-[300px] justify-center bg-white my-6 w-[85%]">
-      <div className="flex items-center justify-center w-1/2">{compChild}</div>
-      <div className="w-1/2 p-2 m-2 bg-gray-50">
+    <div className="flex flex-col min-h-[300px] justify-center bg-white my-6 w-[85%]">
+      <div className="flex items-center justify-center w-full h-1/2">{compChild}</div>
+      <div className="w-full p-2 m-2 h-1/2 bg-gray-50">
         <h3 className="mx-auto text-center ">{title}</h3>
-        <SyntaxHighlighter style={materialLight} wrapLongLines={true} language="jsx">
-          {myCode}
-        </SyntaxHighlighter>
+        <CodeBlock text={myCode} language="jsx" wrapLines />
       </div>
     </div>
   );
