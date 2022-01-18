daf3ddad0906cb6ba28bf0f7fee67052c3d5a693
Arthur Fincham <arthurfincham@me.com>
2022-01-18 12:07:19 +0000

    fix activeclassname error

--- src/siteComponents/Navbar.js
+++ src/siteComponents/Navbar.js
@@ -8,20 +8,20 @@ export default function Navbar() {
   const navShadow = location.pathname.includes('components') || location.pathname.includes('documentation') ? 'md:shadow-md' : 'shadow-md';
   return (
     <>
-      <nav className={`bg-white w-full h-[50px]  px-4 w-full  flex justify-center ${navShadow} fixed`}>
+      <nav className={`bg-white w-full h-[50px]  px-4 w-full  flex justify-center ${navShadow} fixed`} data-testid="navigation">
         <div className="flex items-center w-full navContent">
           <GustIcon svgClass="fill-primary-600 h-[40px]" />
           <div className="flex justify-end w-full ml-6 space-x-4 ">
-            <NavLink exact activeClassName="active" className="mx-2 mt-1 bg-white rounded-md text-md hover:cursor-pointer roboto-black" to="/">
+            <NavLink activeclassname="active" className="mx-2 mt-1 bg-white rounded-md text-md hover:cursor-pointer roboto-black" to="/">
               Home
             </NavLink>
 
-            <NavLink activeClassName="active" className="mx-2 mt-1 bg-white rounded-md text-md hover:cursor-pointer roboto-black" to="/documentation">
+            <NavLink activeclassname="active" className="mx-2 mt-1 bg-white rounded-md text-md hover:cursor-pointer roboto-black" to="/documentation">
               Docs
             </NavLink>
 
             <NavLink
-              activeClassName="active"
+              activeclassname="active"
               className="hidden mx-2 mt-1 bg-white rounded-md text-md md:block hover:cursor-pointer roboto-black"
               to="/components"
             >
@@ -29,7 +29,7 @@ export default function Navbar() {
             </NavLink>
 
             <NavLink
-              activeClassName="active"
+              activeclassname="active"
               className="mx-2 mt-1 bg-white rounded-md md:hidden text-md hover:cursor-pointer roboto-black"
               to="/components"
             >
