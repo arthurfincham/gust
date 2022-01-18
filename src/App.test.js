aa78795aaf52fdfb9343eab561b34ad08ff0ce77
Arthur Fincham <arthurfincham@me.com>
2022-01-18 12:07:05 +0000

    add app rendering test

---
+++ src/App.test.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
+
+import App from './App';
+
+describe('App components render', () => {
+  test('Welcome text', () => {
+    render(<App />);
+    expect(screen.getByText('React and Tailwind CSS')).toBeInTheDocument();
+  });
+
+  test('Navbar', () => {
+    render(<App />);
+    expect(screen.getByTestId('navigation')).toBeInTheDocument();
+  });
+
+  test('Get Started button', () => {
+    render(<App />);
+    expect(screen.getByTestId('getStartedButton')).toBeInTheDocument();
+  });
+
+  test('Components button', () => {
+    render(<App />);
+    expect(screen.getByTestId('componentsButton')).toBeInTheDocument();
+  });
+
+  // test('clears if a correct name is entered', () => {
+  //   const { input } = setup();
+  //   fireEvent.change(input, { target: { value: 'France' } });
+  //   expect(input.value).toBe('');
+  // });
+
+  // test('changes the color of the svg path', () => {
+  //   const { input } = setup();
+  //   const spainPath = screen.getByTitle('Spain');
+  //   fireEvent.change(input, { target: { value: 'Spain' } });
+  //   expect(spainPath.style.fill).toEqual('#FA8F02');
+  // });
+});
