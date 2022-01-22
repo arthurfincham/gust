4f629fdea7cfa55e1c7ad61216fcbeb3769ffcbc
Arthur Fincham <arthurfincham@me.com>
2022-01-22 11:26:33 +0000

    remove commented code

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import { createMemoryHistory } from 'history';
-import { Router } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
 
 import App from './App';
 
@@ -21,17 +18,4 @@ describe('App components render', () => {
     render(<App />);
     expect(screen.getByTestId('desktopAppBanner')).toBeInTheDocument();
   });
-
-  // test('clears if a correct name is entered', () => {
-  //   const { input } = setup();
-  //   fireEvent.change(input, { target: { value: 'France' } });
-  //   expect(input.value).toBe('');
-  // });
-
-  // test('changes the color of the svg path', () => {
-  //   const { input } = setup();
-  //   const spainPath = screen.getByTitle('Spain');
-  //   fireEvent.change(input, { target: { value: 'Spain' } });
-  //   expect(spainPath.style.fill).toEqual('#FA8F02');
-  // });
 });
