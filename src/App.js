f1a7bf50938da0963ccdd742d38adbd53e3e0cdc
Arthur Fincham <arthurfincham@me.com>
2022-01-26 20:29:30 +0000

    add google analytics

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import Components from './siteComponents/Components';
 import Documentation from './siteComponents/Documentation';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import useGoogleAnalytics from './utils/useGoogleAnalytics';
 
-function App() {
+function GARoutes() {
+  useGoogleAnalytics();
   const [isClosed, setIsClosed] = useState(true);
 
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
@@ -17,21 +19,22 @@ function App() {
   }, []);
 
   return (
+    <div className="App scroll-smooth">
+      <Navbar setIsClosed={setIsClosed} isClosed={isClosed} />
+      <main>
+        <Routes>
+          <Route key={1} path="/" element={<Welcome />} />
+          <Route key={2} path="/components" element={<Components isClosed={isClosed} windowWidth={windowWidth} setIsClosed={setIsClosed} />} />
+          <Route key={3} path="/documentation" element={<Documentation isClosed={isClosed} windowWidth={windowWidth} setIsClosed={setIsClosed} />} />
+        </Routes>
+      </main>
+    </div>
+  );
+}
+function App() {
+  return (
     <Router>
-      <div className="App scroll-smooth">
-        <Navbar setIsClosed={setIsClosed} isClosed={isClosed} />
-        <main>
-          <Routes>
-            <Route key={1} path="/" element={<Welcome />} />
-            <Route key={2} path="/components" element={<Components isClosed={isClosed} windowWidth={windowWidth} setIsClosed={setIsClosed} />} />
-            <Route
-              key={3}
-              path="/documentation"
-              element={<Documentation isClosed={isClosed} windowWidth={windowWidth} setIsClosed={setIsClosed} />}
-            />
-          </Routes>
-        </main>
-      </div>
+      <GARoutes />
     </Router>
   );
 }
