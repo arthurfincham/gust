ace36d468ccd1db76ee1716141af13835a8760ee
Arthur Fincham <arthurfincham@me.com>
2022-01-27 11:20:14 +0000

    update comp test to reflect path change

--- src/utils/components.test.js
+++ src/utils/components.test.js
@@ -2,14 +2,16 @@ const fs = require('fs');
 
 describe('Component text file is the same as js', () => {
   test('BasicButton', () => {
-    expect(fs.readFileSync('./src/pubComponents/BasicButton.js')).toEqual(fs.readFileSync('./src/pubComponents/BasicButton.txt'));
+    expect(fs.readFileSync('./src/pubComponents/buttons/BasicButton.js')).toEqual(fs.readFileSync('./src/pubComponents/buttons/BasicButton.txt'));
   });
 
   test('HorizontalProgress', () => {
-    expect(fs.readFileSync('./src/pubComponents/HorizontalProgress.js')).toEqual(fs.readFileSync('./src/pubComponents/HorizontalProgress.txt'));
+    expect(fs.readFileSync('./src/pubComponents/progress/HorizontalProgress.js')).toEqual(
+      fs.readFileSync('./src/pubComponents/progress/HorizontalProgress.txt')
+    );
   });
 
   test('HeaderUnderline', () => {
-    expect(fs.readFileSync('./src/pubComponents/HeaderUnderline.js')).toEqual(fs.readFileSync('./src/pubComponents/HeaderUnderline.txt'));
+    expect(fs.readFileSync('./src/pubComponents/text/HeaderUnderline.js')).toEqual(fs.readFileSync('./src/pubComponents/text/HeaderUnderline.txt'));
   });
 });
