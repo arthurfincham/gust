7dc032e6effcebc85b66e872c638d77ecc3c4ef2
Arthur Fincham <arthurfincham@me.com>
2022-01-27 11:06:39 +0000

    preBuild takes subDir into account, add whitelist

--- src/utils/preBuildScript.js
+++ src/utils/preBuildScript.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
 const folder = './src/pubComponents/';
 
-const handleFile = (file) => {
+const handleFile = (subFolder, file) => {
   const filename = file.substring(0, file.length - 3);
-  var text = fs.readFileSync(`./src/pubComponents/${filename}.js`, 'utf-8');
+  var text = fs.readFileSync(`./src/pubComponents/${subFolder}/${filename}.js`, 'utf-8');
 
-  fs.writeFile(`./src/pubComponents/${filename}.txt`, text, (err) => {
+  fs.writeFile(`./src/pubComponents/${subFolder}/${filename}.txt`, text, (err) => {
     if (err) {
       console.error(err);
       return;
@@ -13,8 +13,15 @@ const handleFile = (file) => {
   });
 };
 
+const dontConvert = ['Buttons.js', 'Cards.js', 'DataDisplay.js', 'Forms.js', 'Lists.js', 'Progress.js', 'Text.js', 'Timeline.js', 'Waves.js'];
+
 fs.readdirSync(folder)
-  .filter((x) => x.endsWith('.js'))
-  .forEach((file) => {
-    handleFile(file);
+  .filter((x) => x !== 'AllComponents.js')
+  .forEach((subFolder) => {
+    fs.readdirSync(`${folder}${subFolder}/`)
+      .filter((x) => x.endsWith('.js'))
+      .filter((x) => !dontConvert.includes(x))
+      .forEach((file) => {
+        handleFile(subFolder, file);
+      });
   });
